135e50cfae27432fa9eb7d7a4eaee3588e7d6fb8
ShemarS <42276240+ShemarS@users.noreply.github.com>
2022-03-22 19:17:41 -0400

    commit
    fragment stuff

--- app/src/main/java/com/example/fragmenthwassignment/MainActivity.kt
+++ app/src/main/java/com/example/fragmenthwassignment/MainActivity.kt
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val transaction = supportFragmentManager.beginTransaction()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
